b25311e1cab7b1883e3b257e32e9231320dd083a
rufevean <deerajmydearalways@gmail.com>
2025-06-06 20:47:15 +0530

    refactor - removed userId and changed controller according to new changes .

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,8 @@ const userRouter = express.Router();
 
 
 userRouter.post('/create', userController.createNewUser); 
-userRouter.post('/update/:id', userController.updateUser); 
-userRouter.post("/delete/:id", userController.deleteUser); 
+userRouter.post('/update', userController.updateUser); 
+userRouter.post("/delete", userController.deleteUser); 
 userRouter.get('/getAll', userController.getAllUsers); 
 userRouter.get('/:username', userController.getUserByUsername);
 
